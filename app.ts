4b421db7b3b4771be31bf8b2d29644cffd4b7aad
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-12 23:05:27 +0530

    :sparkles: metaplex finally works

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
-// import packages
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
-
-// import routers
+import { PublicKey } from "@solana/web3.js";
+import metaplex from "./utils/metaplex";
 
 const app = express();
 
@@ -13,5 +12,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use(morgan("dev"));
+
+app.get("/all/:id", async (req: Request, res: Response) => {
+	const myNfts = await metaplex
+		.nfts()
+		.findAllByOwner(new PublicKey(req.params.id))
+		.run();
+
+	return res.json({
+		data: myNfts,
+	});
+});
+
+app.post("/mint", async (req: Request, res: Response) => {
+	const { name, description, url } = req.body;
+
+	const { uri } = await metaplex.nfts().uploadMetadata({
+		name,
+		description,
+		image: url,
+	});
+
+	const { nft } = await metaplex.nfts().create({
+		uri: uri,
+	});
+
+	console.log(nft.mint.toBase58());
+});
 
 export default app;
--- utils/metaplex.ts
+++ utils/metaplex.ts
@@ -1,19 +1,7 @@
-import {
-	Metaplex,
-	keypairIdentity,
-	bundlrStorage,
-} from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
-import fs from "fs";
-
-const pathToMyKeypair = process.env.HOME + "/.config/solana/id.json";
-const keypairFile = fs.readFileSync(pathToMyKeypair);
-const secretKey = Buffer.from(JSON.parse(keypairFile.toString()));
-const myKeyPair = Keypair.fromSecretKey(secretKey);
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, clusterApiUrl } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
-const metaplex = Metaplex.make(connection)
-	.use(keypairIdentity(myKeyPair))
-	.use(bundlrStorage({ address: "https://devnet.bundlr.network" }));
+const metaplex = new Metaplex(connection);
 
 export default metaplex;
